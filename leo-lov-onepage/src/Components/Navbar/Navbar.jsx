252e88668e7387e9ec5f7e4159a1343c385bfb54
simo994q <simonkn1@hotmail.com>
2023-08-14 09:49:07 +0200

    ikke funktionel nav

--- leo-lov-onepage/src/Components/Navbar/Navbar.jsx
+++ leo-lov-onepage/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 import style from './Navbar.module.scss'
 
 export function Navbar () {
+    function toggleNavbar () {
+        
+    }
+
     return (
         <div className={style.navbarContainer}>
 
-            <ul>
+            <div onClick={toggleNavbar}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            
+            <ul className={style.navbarList}>
                 <li><a href='#'>Hjem</a></li>
                 <li><a href='#'>Advokaterne</a></li>
                 <li><a href='#'>Om LeoLov</a></li>
